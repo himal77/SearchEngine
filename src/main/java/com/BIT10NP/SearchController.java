4771148d46499d062ce98aebe78802e8e40aa4d0
himal77 <himalpuri6@gmail.com>
2020-09-22 18:22:35 +0200

    FontPart With bootstrap

---
+++ src/main/java/com/BIT10NP/SearchController.java
@@ -0,0 +1,13 @@
+package com.BIT10NP;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SearchController {
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
